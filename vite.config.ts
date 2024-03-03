844682e1b8a76b950dbcfc04f13519e03cddf526
Michael Mleczek <michalmleczek@outlook.com>
2024-03-03 19:07:23 +0100

    Domain changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "https://www.recreatio.eu",
+    base: "https://recreatio.eu",
   plugins: [react()],
 })
