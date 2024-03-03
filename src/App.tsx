6968356ab28b8b60a576ac4ee6c2bbd7fe4288ca
Michael Mleczek <michalmleczek@outlook.com>
2024-03-03 20:21:29 +0100

    Eliminated some errors

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import FinanceComponent from './temp/FinanceComponent';
-
+interface JSON_Object {
+    id: string;
+    output: number;
+}
 function App() {
     const [count, setCount] = useState(0);
-    const [data, setData] = useState([])
+    const [data, setData] = useState([] as JSON_Object[])
     const [sum, setSum] = useState(0)
     const formatter = new Intl.NumberFormat('pl-PL', {style: 'currency', currency: 'PLN'});
 
@@ -14,7 +17,7 @@ function App() {
     useEffect(() => {
         (async () => {
             const data = await fetch('https://zapp.hostingasp.pl/information/integer/G6XLgrfsEAIR21t7RgNsgP84UeGeM9QWkq4j6tycNjw/finanseministrantow')
-                .then(res => res.json())
+                .then(res => res.json() as unknown as JSON_Object[])
             setData(data)
             let tempSum = 0
             for (let index = 0; index < data.length; index++)
@@ -54,7 +57,7 @@ function App() {
           <div>
               {
                   data.map(item => (
-                  <FinanceComponent key={item.id} information_id={item.id} amount={item.output} />
+                <FinanceComponent key={item.id} information_id={item.id} amount={item.output} />
                   ))
               }
           </div>
