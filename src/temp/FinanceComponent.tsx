0c5d3404422d5da8e28fd55c6b87061d96b57287
Michael Mleczek <michalmleczek@outlook.com>
2024-03-03 19:48:03 +0100

    revert

--- src/temp/FinanceComponent.tsx
+++ src/temp/FinanceComponent.tsx
@@ -1,20 +1,20 @@
 import { useState, useEffect } from 'react'
 
 
-function FinanceComponent({ information_id, amount }) {
+function FinanceComponent({ information_id, amount }: { information_id: string, amount: number }) {
     const formatter = new Intl.NumberFormat('pl-PL', { style: 'currency', currency: 'PLN' });
-    const [data, setData] = useState([]);
+    const [name, setName] = useState("");
 
     useEffect(() => {
         (async () => {
             const data = await fetch('https://zapp.hostingasp.pl/information/text/G6XLgrfsEAIR21t7RgNsgP84UeGeM9QWkq4j6tycNjw/' + information_id + 'name')
                 .then(res => res.json());
-            setData(data[0]);
+            setName((data[0]).output);
         })()
     }, [])
 
     return (
-        <div title={data.output}>
+        <div title={name}>
             <p>{formatter.format(amount / 100)}</p>
         </div>
   );
