cfc1e6704451c9b9c1d74e86ba7ba8a4d65007d5
Michael Mleczek <michalmleczek@outlook.com>
2024-03-05 11:35:05 +0100

    Resolved an ts issue

--- src/routes/error-page.tsx
+++ src/routes/error-page.tsx
@@ -9,7 +9,7 @@ export default function ErrorPage() {
             <h1>Oops!</h1>
             <p>Sorry, an unexpected error has occurred.</p>
             <p>
-                <i>{error.name || error.message}</i>
+                <i></i>
             </p>
         </div>
     );
