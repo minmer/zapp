77defab2391a06c8e3fb9c7a8351fae1c1b7292d
Michael Mleczek <michalmleczek@outlook.com>
2024-03-06 22:51:58 +0100

    https added

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "http://www.recreatio.eu",
+    base: "https://www.recreatio.eu",
   plugins: [react()],
 })
