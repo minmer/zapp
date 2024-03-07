3d013cb82e1552309486a3cabd3afa5e5254e237
Michael Mleczek <michalmleczek@outlook.com>
2024-03-07 10:11:28 +0100

    Removed unused const

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,7 +1,6 @@
-import { Outlet, Link, useParams } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 export default function Root() {
-
-    const { token } = useParams();
+    
     return (
         <>
             <div id="sidebar">
