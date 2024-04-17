cd9bbe40d52ab095b5ba28a368ce78a949ab8d21
Michael Mleczek <michalmleczek@outlook.com>
2024-04-17 13:38:39 +0200

    Added message to reload token

--- src/components/users-detail-component.tsx
+++ src/components/users-detail-component.tsx
@@ -1,17 +1,21 @@
 import { useParams } from "react-router-dom";
 import { FetchReloadToken } from "../features/FetchReloadToken";
+import { useState } from "react";
 
 export default function UsersDetailElement() {
     const { token } = useParams();
+    const [message, setMessage] = useState("");
 
     const reloadWallet = () => {
+        setMessage("")
         console.log(FetchReloadToken(token ?? ''))
-
+        setMessage("Reloaded wallet")
     }
 
     return (
         <>
             <input type="button" onClick={reloadWallet} value="Reload Wallet" />
+            {message}
         </>
     );
 }
