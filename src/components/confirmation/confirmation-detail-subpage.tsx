2a023bd183031d4de5626e436c0466338e2c559a
Michael Mleczek <michalmleczek@outlook.com>
2024-08-27 09:25:31 +0200

    Fixed issues with detail subpage

--- src/components/confirmation/confirmation-detail-subpage.tsx
+++ src/components/confirmation/confirmation-detail-subpage.tsx
@@ -1,36 +1,4 @@
-import { useEffect, useState } from "react";
-import EditableElement from "../../generals/editable-element";
-import { CreateRole, GetRole, Role } from "../../structs/role";
-import { User } from "../../structs/user";
-
-export default function ConfirmationDetailSubpage({ getParams }: { getParams: ({ func, type, show }: { func: (t: unknown) => Promise<unknown>, type: string, show: boolean }) => Promise<unknown> }) {
-
-    const [selectedUser, setSelectedUser] = useState<User>()
-    const [role, setRole] = useState<Role | null>()
-    useEffect(() => {
-        (async function () {
-            //await getParams({ func: async (user: string, id: string) => setSelectedUser({ user, id }) })
-        })();
-    }, [getParams])
-    const selectUser = async () => {
-        //setSelectedUser(await getParams({ func: async (user: string, id: string) => setSelectedUser({ user, id }) }) as User)
-    }
-    useEffect(() => {
-        (async function () {
-            if (selectedUser != null) {
-                setRole(await GetRole({ getParams: getParams, type: "confirmation", user: selectedUser }))
-            }
-        }());
-    }, [getParams, selectedUser])
-
-    const register = () => {
-        (async function () {
-            if (selectedUser != null) {
-                setRole(await CreateRole({ getParams: getParams, type: "confirmation", user: selectedUser }))
-            }
-        })();
-    }
-
+export default function ConfirmationDetailSubpage() {
     return (
         <>
         <h3>Twoje zgłoszenie zostało wysłane i oczekuje na przyjęcie</h3>
