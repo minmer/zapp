6d1e739759603d5764ac16145cc8df643bcc5d22
Michael Mleczek <michalmleczek@outlook.com>
2024-12-16 03:16:14 +0100

    Added some streets

--- src/components/visit/VisitCheckSubpage.tsx
+++ src/components/visit/VisitCheckSubpage.tsx
@@ -4,6 +4,9 @@ import { User } from "../../structs/user";
 export default function VisitCheckSubpage() {
     return (
         <>
+            <div className="description">
+                <p>W trakcie przygotowania</p>
+            </div>
         </>
     );
 }
