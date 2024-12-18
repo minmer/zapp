40252b3d6da56c51769871b558af08f4771ef9a9
Michael Mleczek <michalmleczek@outlook.com>
2024-12-18 12:27:07 +0100

    Added pdf

--- src/pages/advent-page.tsx
+++ src/pages/advent-page.tsx
@@ -264,6 +264,12 @@ export default function AdventPage() {
                             display: 'inline-block',
                             width: 'auto',
                         }}>17.12.2024 r.</a>
+                        <a href="https://youtu.be/81-tqzl57Jc" target="_blank" style={{
+
+                            margin: '6px',
+                            display: 'inline-block',
+                            width: 'auto',
+                        }}>18.12.2024 r.</a>
                     </div>
                     <h2>Materiały</h2>
                     <div style={{
@@ -379,6 +385,12 @@ export default function AdventPage() {
                             display: 'inline-block',
                             width: 'auto',
                         }}>17.12.2024 r.</a>
+                        <a href="https://www.recreatio.eu/roraty/18.pdf" target="_blank" style={{
+
+                            margin: '6px',
+                            display: 'inline-block',
+                            width: 'auto',
+                        }}>18.12.2024 r.</a>
                     </div>                    <h2>Roratni turniej szachowy</h2>
                     <div
                         style={{
